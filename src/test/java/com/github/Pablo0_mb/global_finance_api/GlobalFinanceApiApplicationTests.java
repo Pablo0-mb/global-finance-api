7571a56a4c62f9378e69823032fc06cae1f78292
Pablo Matanza <pablo.matanza@edu.uah.es>
2026-01-14 17:23:26 +0100

    chore(SETUP & INFRA)

---
+++ src/test/java/com/github/Pablo0_mb/global_finance_api/GlobalFinanceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Pablo0_mb.global_finance_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalFinanceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
